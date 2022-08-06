67e8a833ec3188b0615329a3e8b86a1cfabe44cc
Shrirang Mahajan <85283622+NotShrirang@users.noreply.github.com>
2022-08-06 08:59:32 +0530

    Update sentiment_analysis_testing.py

--- sentiment_module/sentiment_analysis_testing.py
+++ sentiment_module/sentiment_analysis_testing.py
@@ -1,3 +1,7 @@
 import sentiment_mod as s
 
 print(s.sentiment("It was great movie! I liked it a lot! Lots of jokes and punchlines!"))
+
+# Output:
+# voted_classifier accuracy percent: 76.20481927710844
+# ('pos', 1.0)
